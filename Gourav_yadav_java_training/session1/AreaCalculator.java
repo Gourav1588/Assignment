53fe1cc5ec09499d666baf6e99334d706bc9caa6
Gourav Yadav <you@exam>
2026-04-07 20:26:27 +0530

    feat : make the structure for the project

---
+++ Gourav_yadav_java_training/session1/AreaCalculator.java
@@ -0,0 +1,18 @@
+package Gourav_yadav_java_training.session1;
+
+
+public class AreaCalculator {
+    private static final double PI = 3.14159; 
+
+    public double calculateCircleArea(double radius) {
+        return PI * radius * radius;
+    }
+
+    public double calculateRectangleArea(double length, double width) {
+        return length * width;
+    }
+
+    public double calculateTriangleArea(double base, double height) {
+        return 0.5 * base * height;
+    }
+}
