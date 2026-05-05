dedcd26264b4dd57806af0dbebbe75f635a7f43d
Gourav Yadav <you@exam>
2026-05-05 19:53:26 +0530

    test: update RentalApplicationTests

--- Capstone_VehicleRentalSystem/backend/rental/src/main/java/com/vehicle/rental/RentalApplication.java
+++ Capstone_VehicleRentalSystem/backend/rental/src/main/java/com/vehicle/rental/RentalApplication.java
@@ -2,7 +2,7 @@ package com.vehicle.rental;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootApplication
 
